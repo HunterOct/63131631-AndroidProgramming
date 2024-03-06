cc782f5fd6bff6d794fa7817fc53f5e603981492
Hữu Tường <hunter.19oct@gmail.com>
2024-03-06 14:11:17 +0700

    Update

--- Ex3_SimpleSumApp/app/src/main/java/com/example/ex3_simplesumapp/MainActivity.java
+++ Ex3_SimpleSumApp/app/src/main/java/com/example/ex3_simplesumapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.ex3_simplesumapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public  void Xulycong(View view){
+        // tham chiếu điều khiển trên tệp xml
+        EditText edittextA = findViewById(R.id.editA);
+        EditText edittextB = findViewById(R.id.editB);
+        EditText edittextKQ = findViewById(R.id.editKQ);
+        // lấy dữ liêu từ ô chuyển sang string
+        String soA = edittextA.getText().toString();
+        String soB = edittextB.getText().toString();
+        // chuyển dữ liêu sang số
+        int so_a = Integer.parseInt(soA);
+        int so_b = Integer.parseInt(soB);
+        // Tính toán
+        int tong = so_a + so_b;
+        // hiện kq ra màn hình
+        // Đổi số sang chuôi
+        String strKQ = String.valueOf(tong);
+        edittextKQ.setText(strKQ);
+
     }
 }
