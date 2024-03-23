a64c0b6f5f9a087faf295d5c11458739d57c23e0
Hữu Tường <hunter.19oct@gmail.com>
2024-03-23 22:11:38 +0700

    Add Button

--- Bai2_GhiChu/app/build.gradle.kts
+++ Bai2_GhiChu/app/build.gradle.kts
@@ -37,6 +37,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(com.google.android.material.button.MaterialButton)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
