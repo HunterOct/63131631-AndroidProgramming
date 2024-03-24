2baeb562e4d54a1b87765bce8a302df5d42b3ff5
Hữu Tường <hunter.19oct@gmail.com>
2024-03-24 19:05:50 +0700

    Update frBMI.java

--- Cau1_TinhBMI/ntu_63131631/src/ntu_63131631/frBMI.java
+++ Cau1_TinhBMI/ntu_63131631/src/ntu_63131631/frBMI.java
@@ -12,25 +12,6 @@ public class frBMI extends JFrame {
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					frBMI frame = new frBMI();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the frame.
-	 */
 	public frBMI() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
