b248c6b11064ce64a587a46c0b3abc103fb0449a
Hữu Tường <hunter.19oct@gmail.com>
2024-03-24 19:40:47 +0700

    Update NoteAdapter.java

--- Cau2_GhiChu/app/src/main/java/ntu_63131631/cau2_ghichu/NoteAdapter.java
+++ Cau2_GhiChu/app/src/main/java/ntu_63131631/cau2_ghichu/NoteAdapter.java
@@ -4,11 +4,12 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
-public class NoteAdapter {
+public class NoteAdapter extends ArrayAdapter<String> {
     public NoteAdapter(Context context, ArrayList<String> notes) {
         super(context, 0, notes);
     }
@@ -33,4 +34,3 @@ public class NoteAdapter {
 
         return convertView;
     }
-}
