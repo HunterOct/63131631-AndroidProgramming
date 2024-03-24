aea2bef21b692d483da043b8c7de7cc53ec11ad0
Hữu Tường <hunter.19oct@gmail.com>
2024-03-24 20:53:50 +0700

    Update Icon

--- Cau2_GhiChu/app/src/main/java/ntu_63131631/cau2_ghichu/MainActivity.java
+++ Cau2_GhiChu/app/src/main/java/ntu_63131631/cau2_ghichu/MainActivity.java
@@ -1,14 +1,21 @@
 package ntu_63131631.cau2_ghichu;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
+import com.google.android.material.textfield.TextInputEditText;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
     ListView lv_Notes;
@@ -32,5 +39,38 @@ public class MainActivity extends AppCompatActivity {
         //custom adapter
         nguonDuLieu = new NoteAdapter(this,dsNote);
         lv_Notes.setAdapter(nguonDuLieu);
+
+        btnNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                View view1 = LayoutInflater.from(MainActivity.this).inflate(R.layout.dialog_layout, null);
+                // lấy dữ liệu từ dialog
+                TextInputEditText editTextTitle = view1.findViewById(R.id.edit_text_title);
+                TextInputEditText editTextNotes = view1.findViewById(R.id.edit_text_notes);
+                AlertDialog alertDialog = new MaterialAlertDialogBuilder(MainActivity.this)
+                        .setTitle("Ghi Chú Mới")
+                        .setView(view1)
+                        .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                //Nhập và lưu ghi chú
+                                String title = Objects.requireNonNull(editTextTitle.getText()).toString();
+                                String notes = Objects.requireNonNull(editTextNotes.getText()).toString();
+                                dsNote.add(title + "\n" + notes);
+                                if (!dsNote.isEmpty()) {
+                                    findViewById(R.id.cs_background).setVisibility(View.GONE);
+                                }
+                                nguonDuLieu.notifyDataSetChanged(); // Thông báo cho adapter rằng dữ liệu đã thay đổi
+                                dialogInterface.dismiss();
+                            }
+                        }).setNegativeButton("Huỷ Bỏ", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                dialogInterface.dismiss();
+                            }
+                        }).create();
+                alertDialog.show();
+            }
+        });
     }
 }
