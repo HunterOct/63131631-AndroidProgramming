9eabcac1aff042d4eb3454a03ee1c417f39930b1
Hữu Tường <hunter.19oct@gmail.com>
2024-03-24 18:26:26 +0700

    Update frBMI.java

--- Bai1_TinhBMI/ntu_63131631/src/ntu_63131631/frBMI.java
+++ Bai1_TinhBMI/ntu_63131631/src/ntu_63131631/frBMI.java
@@ -6,11 +6,22 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import java.awt.SystemColor;
+import java.awt.Color;
+import javax.swing.JTextField;
+import javax.swing.JTextPane;
+import javax.swing.JButton;
 
 public class frBMI extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
 
 	/**
 	 * Launch the application.
@@ -33,11 +44,69 @@ public class frBMI extends JFrame {
 	 */
 	public frBMI() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 492, 526);
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(255, 250, 205));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(0, 0, 478, 102);
+		panel.setBackground(new Color(241, 196, 15));
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel_2 = new JLabel("Ứng Dụng Tính BMI");
+		lblNewLabel_2.setBounds(42, 25, 394, 49);
+		panel.add(lblNewLabel_2);
+		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.BOLD, 40));
+		
+		JLabel lblNewLabel_1 = new JLabel("Chiều Cao:");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		lblNewLabel_1.setBounds(25, 134, 139, 38);
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_1_1 = new JLabel("Cân Nặng:");
+		lblNewLabel_1_1.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		lblNewLabel_1_1.setBounds(25, 192, 139, 38);
+		contentPane.add(lblNewLabel_1_1);
+		
+		JLabel lblNewLabel_1_2 = new JLabel("Chỉ Số BMI Của Bạn:");
+		lblNewLabel_1_2.setFont(new Font("Tahoma", Font.BOLD, 25));
+		lblNewLabel_1_2.setBounds(25, 311, 272, 38);
+		contentPane.add(lblNewLabel_1_2);
+		
+		textField = new JTextField();
+		textField.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		textField.setBounds(163, 134, 272, 33);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		textField_1.setColumns(10);
+		textField_1.setBounds(163, 192, 272, 33);
+		contentPane.add(textField_1);
+		
+		textField_2 = new JTextField();
+		textField_2.setHorizontalAlignment(SwingConstants.CENTER);
+		textField_2.setEnabled(false);
+		textField_2.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		textField_2.setColumns(10);
+		textField_2.setBounds(307, 314, 128, 33);
+		contentPane.add(textField_2);
+		
+		JButton btnNewButton = new JButton("Tính");
+		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		btnNewButton.setBounds(163, 240, 116, 43);
+		contentPane.add(btnNewButton);
+		
+		JButton btnThot = new JButton("Thoát");
+		btnThot.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		btnThot.setBounds(319, 240, 116, 43);
+		contentPane.add(btnThot);
 	}
-
 }
