1798035c1c56b0751fbc6d1df6300e55fb976773
Hữu Tường <hunter.19oct@gmail.com>
2024-03-24 19:34:46 +0700

    Update

--- Cau2_GhiChu/app/src/main/java/ntu_63131631/cau2_ghichu/MainActivity.java
+++ Cau2_GhiChu/app/src/main/java/ntu_63131631/cau2_ghichu/MainActivity.java
@@ -1,12 +1,15 @@
 package ntu_63131631.cau2_ghichu;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,5 +22,12 @@ public class MainActivity extends AppCompatActivity {
             getSupportActionBar().hide();
         }
         setContentView(R.layout.activity_main);
+
+        btnNote = findViewById(R.id.btn_add_note);
+        lv_Notes = (ListView) findViewById(R.id.list_view_notes);
+        dsNote = new ArrayList<String>();
+        //custom adapter
+        nguonDuLieu = new NotesAdapter(this,dsNote);
+        lv_Notes.setAdapter(nguonDuLieu);
     }
 }
