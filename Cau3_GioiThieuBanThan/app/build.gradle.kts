3a8e962ffc50e3c1efd2f34e322150a7c0af159b
Hữu Tường <hunter.19oct@gmail.com>
2024-03-25 10:11:13 +0700

    Update

--- Cau3_GioiThieuBanThan/app/build.gradle.kts
+++ Cau3_GioiThieuBanThan/app/build.gradle.kts
@@ -37,6 +37,8 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation("androidx.compose.material:material:1.6.4")
+    implementation("androidx.cardview:cardview:1.0.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
--- Cau3_GioiThieuBanThan/app/src/main/java/ntu_63131631/cau3_gioithieubanthan/MainActivity.java
+++ Cau3_GioiThieuBanThan/app/src/main/java/ntu_63131631/cau3_gioithieubanthan/MainActivity.java
@@ -1,6 +1,7 @@
 package ntu_63131631.cau3_gioithieubanthan;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +21,6 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
     }
 }
