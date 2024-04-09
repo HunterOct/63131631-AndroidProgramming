71a4177dbf8cb60f0d271719da25b6639f2a2165
Hữu Tường <hunter.19oct@gmail.com>
2024-04-09 13:53:21 +0700

    Update

---
+++ UsingRecyclerView/app/src/main/java/ntu/huutuong/usingrecyclerview/LandScapeAdapter.java
@@ -0,0 +1,57 @@
+package ntu.huutuong.usingrecyclerview;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.zip.Inflater;
+
+public class LandScapeAdapter extends RecyclerView.Adapter<LandScapeAdapter.ItemLandHolder> {
+    Context context;
+    ArrayList<LandScape> lstData;
+
+    public LandScapeAdapter(Context context, ArrayList<LandScape> lstData) {
+        this.context = context;
+        this.lstData = lstData;
+    }
+
+    @NonNull
+    @Override
+    public ItemLandHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater cai_bom = LayoutInflater.from(context);
+        View viewItem = cai_bom.inflate(R.layout.item_land, parent, false);
+        ItemLandHolder viewholderCreated = new ItemLandHolder(viewItem);
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ItemLandHolder holder, int position) {
+        // Lấy Đối Tượng Hiển Thị
+        LandScape landScapeHienThi = lstData.get(position);
+        // TRích Thông tin
+        String caption = landScapeHienThi.getLanCation();
+        String tenAnh = landScapeHienThi.getLandImageName();
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    class ItemLandHolder extends RecyclerView.ViewHolder {
+        TextView tvCaption;
+        ImageView ivLandScape;
+        public ItemLandHolder(@NonNull View itemView) {
+            super(itemView);
+            tvCaption = itemView.findViewById(R.id.TextViewCation);
+            ivLandScape = itemView.findViewById(R.id.ImageViewland);
+        }
+    }
+}
