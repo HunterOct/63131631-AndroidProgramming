9f8c11b994d2660d9831880694e8b65cfe1dcfe1
Hữu Tường <hunter.19oct@gmail.com>
2024-04-15 15:11:26 +0700

    Add examData Class

---
+++ ExamList/app/src/main/java/ntu/huutuong/examlist/examData.java
@@ -0,0 +1,18 @@
+package ntu.huutuong.examlist;
+
+package com.prodigieux.admin.prodigieux;
+
+public class examData {
+    String name;
+    String date;
+    String message;
+
+    examData(String name,
+             String date,
+             String message)
+    {
+        this.name = name;
+        this.date = date;
+        this.message = message;
+    }
+}
