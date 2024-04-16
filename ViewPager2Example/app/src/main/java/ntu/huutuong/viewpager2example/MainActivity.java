4265e07dd62444c972a7e8b0c364200a74f9083f
Hữu Tường <hunter.19oct@gmail.com>
2024-04-16 10:49:29 +0700

    Update

--- ViewPager2Example/app/src/main/java/ntu/huutuong/viewpager2example/MainActivity.java
+++ ViewPager2Example/app/src/main/java/ntu/huutuong/viewpager2example/MainActivity.java
@@ -1,24 +1,59 @@
 package ntu.huutuong.viewpager2example;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        ViewPager2 viewPager = findViewById(R.id.viewPager);
+        viewPager.setAdapter(new ViewPagerAdapter(this));
+        viewPager.setPageTransformer(new ViewPager2.PageTransformer() {
+            @Override
+            public void transformPage(@NonNull View page, float position) {
+                final float MIN_SCALE = 0.75f;
+                int pageWidth = page.getWidth();
+                if (position < -1) {
+                    page.setAlpha(0f);
+                } else if (position <= 0) {
+                    page.setAlpha(1f);
+                    page.setTranslationX(0f);
+                    page.setScaleX(1f);
+                    page.setScaleY(1f);
+                } else if (position <= 1) {
+                    page.setAlpha(1 - position);
+                    page.setTranslationX(pageWidth * -position);
+                    float scaleFactor = MIN_SCALE
+                            + (1 - MIN_SCALE) * (1 - Math.abs(position));
+                    page.setScaleX(scaleFactor);
+                    page.setScaleY(scaleFactor);
+                } else {
+                    page.setAlpha(0f);
+                }
+            }
         });
+//        TabLayout tabLayout = findViewById(R.id.tab_layout);
+//        TabLayoutMediator tabLayoutMediator = new TabLayoutMediator(
+//                tabLayout, viewPager, true, new TabLayoutMediator.TabConfigurationStrategy() {
+//            @Override
+//            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+//                tab.setText("Tab " + (position + 1));
+//            }
+//        });
+//        tabLayoutMediator.attach();
     }
+
 }
