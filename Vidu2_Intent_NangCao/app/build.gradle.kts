ffc50566cdf24eece84207d9a55b554396af91bc
Hữu Tường <hunter.19oct@gmail.com>
2024-04-28 16:29:03 +0700

    Add NhapLieu Activity

--- Vidu2_Intent_NangCao/app/build.gradle.kts
+++ Vidu2_Intent_NangCao/app/build.gradle.kts
@@ -36,6 +36,7 @@ dependencies {
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
+    implementation("com.google.android.material:material:<version>")
     implementation(libs.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
