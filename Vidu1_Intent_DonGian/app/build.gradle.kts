7faedc4c1120f931f4c991b690ceb269765b29f6
Hữu Tường <hunter.19oct@gmail.com>
2024-04-28 14:59:15 +0700

    Update

--- Vidu1_Intent_DonGian/app/build.gradle.kts
+++ Vidu1_Intent_DonGian/app/build.gradle.kts
@@ -36,6 +36,7 @@ dependencies {
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
+    implementation("androidx.compose.material:material:1.6.4")
     implementation(libs.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
