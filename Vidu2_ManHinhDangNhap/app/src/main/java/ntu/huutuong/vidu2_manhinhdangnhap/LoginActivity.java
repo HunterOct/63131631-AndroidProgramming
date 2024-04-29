15b4aa78cb8594c891803a868548eb5293b1f8f3
Hữu Tường <hunter.19oct@gmail.com>
2024-04-29 21:12:16 +0700

    Update LoginActivity.java

--- Vidu2_ManHinhDangNhap/app/src/main/java/ntu/huutuong/vidu2_manhinhdangnhap/LoginActivity.java
+++ Vidu2_ManHinhDangNhap/app/src/main/java/ntu/huutuong/vidu2_manhinhdangnhap/LoginActivity.java
@@ -3,6 +3,8 @@ package ntu.huutuong.vidu2_manhinhdangnhap;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,8 +24,20 @@ public class LoginActivity extends AppCompatActivity {
 //            return insets;
 //        });
     }
-    public void XacNhan(View view) {
-        Intent xacNhan = new Intent(this, HomeActivity.class);
-        startActivity(xacNhan);
+    public void XacNhan(View v){
+        EditText edtUsername = ((EditText) findViewById(R.id.edtUsername));
+        EditText edtPass = ((EditText) findViewById(R.id.edtPass));
+        EditText edtEmail = ((EditText) findViewById(R.id.edtEmail));
+        String userName = edtUsername.getText().toString();
+        String userPass = edtPass.getText().toString();
+        String userEmail = edtEmail.getText().toString();
+        if(userName.equals("63CNTT2") && userPass.equals("ABCDEF")){
+            Intent iHome = new Intent(this, HomeActivity.class);
+            startActivity(iHome);
+        }
+        else{
+            Toast.makeText(this, "Đăng Nhập Thất Bại", Toast.LENGTH_LONG).show();
+        }
+        finish();
     }
 }
