c5c51e8d6d946145f00feeccaed78dd8c85e1b41
Hữu Tường <hunter.19oct@gmail.com>
2024-04-29 17:23:02 +0700

    Fix Color Sync

--- Vidu2_ManHinhDangNhap/app/src/main/java/ntu/huutuong/vidu2_manhinhdangnhap/LoginActivity.java
+++ Vidu2_ManHinhDangNhap/app/src/main/java/ntu/huutuong/vidu2_manhinhdangnhap/LoginActivity.java
@@ -1,6 +1,8 @@
 package ntu.huutuong.vidu2_manhinhdangnhap;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +11,19 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class LoginActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+    }
+    public void XacNhan(View view) {
+        Intent xacNhan = new Intent(this, HomeActivity.class);
+        startActivity(xacNhan);
     }
 }
