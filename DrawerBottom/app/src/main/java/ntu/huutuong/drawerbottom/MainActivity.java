732cf942c7ae22d4c1aeae4a1dd83eec9b317009
Hữu Tường <hunter.19oct@gmail.com>
2024-06-01 00:39:30 +0700

    Update

--- DrawerBottom/app/src/main/java/ntu/huutuong/drawerbottom/MainActivity.java
+++ DrawerBottom/app/src/main/java/ntu/huutuong/drawerbottom/MainActivity.java
@@ -1,5 +1,7 @@
 package ntu.huutuong.drawerbottom;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
 import androidx.annotation.NonNull;
@@ -48,24 +50,38 @@ public class MainActivity extends AppCompatActivity {
                 if (itemid == R.id.drawer_home) {
                     selectedFragment = new HomeFragment();
                     bottomNavigationView.setSelectedItemId(R.id.navigation_home);
-                } else if (itemid == R.id.drawer_dashboard) {
+                } else if (itemid == R.id.drawer_list) {
                     selectedFragment = new RecycerviewFragment();
-                    bottomNavigationView.setSelectedItemId(R.id.navigation_dashboard);
-                } else if (itemid == R.id.drawer_notifications) {
+                    bottomNavigationView.setSelectedItemId(R.id.navigation_list);
+                } else if (itemid == R.id.drawer_profile) {
                     selectedFragment = new ProfileFragment();
-                    bottomNavigationView.setSelectedItemId(R.id.navigation_notifications);
+                    bottomNavigationView.setSelectedItemId(R.id.navigation_profile);
                 } else if (itemid == R.id.drawer_bmi) {
                     selectedFragment = new BMIFragment();
-                    bottomNavigationView.setSelectedItemId(R.id.navigation_bmi);
                 } else if (itemid == R.id.drawer_caculator) {
                     selectedFragment = new CaculatorFragment();
                     bottomNavigationView.setSelectedItemId(R.id.navigation_caculator);
+                } else if (itemid == R.id.drawer_github) {
+                    // Chuyển hướng đến trang GitHub của bạn
+                    String githubUrl = "https://github.com/HunterOct";
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(githubUrl));
+                    startActivity(intent);
+                    drawerLayout.closeDrawers();
+                    return true;
+                } else if (itemid == R.id.drawer_facebook) {
+                    // Chuyển hướng đến trang Facebook của bạn
+                    String facebookUrl = "https://www.facebook.com/HunterDz.1910";
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(facebookUrl));
+                    startActivity(intent);
+                    drawerLayout.closeDrawers();
+                    return true;
                 }
-                getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment, selectedFragment).commit();
+
                 if (selectedFragment != null) {
                     getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment, selectedFragment).commit();
+                    toolbar.setTitle(item.getTitle());
                 }
-                toolbar.setTitle(item.getTitle());
+
                 drawerLayout.closeDrawers();
                 return true;
             }
@@ -80,19 +96,22 @@ public class MainActivity extends AppCompatActivity {
                 if (itemid == R.id.navigation_home) {
                     selectedFragment = new HomeFragment();
                     navigationView.setCheckedItem(R.id.drawer_home);
-                } else if (itemid == R.id.navigation_dashboard) {
+                } else if (itemid == R.id.navigation_list) {
                     selectedFragment = new RecycerviewFragment();
-                    navigationView.setCheckedItem(R.id.drawer_dashboard);
-                } else if (itemid == R.id.navigation_notifications) {
+                    navigationView.setCheckedItem(R.id.drawer_list);
+                } else if (itemid == R.id.navigation_profile) {
                     selectedFragment = new ProfileFragment();
-                    navigationView.setCheckedItem(R.id.drawer_notifications);
-                } else if (itemid == R.id.navigation_bmi) {
-                    selectedFragment = new BMIFragment();
-                    navigationView.setCheckedItem(R.id.drawer_bmi);
-                }   else if (itemid == R.id.navigation_caculator) {
+                    navigationView.setCheckedItem(R.id.drawer_profile);
+                }
+//                else if (itemid == R.id.navigation_bmi) {
+//                    selectedFragment = new BMIFragment();
+//                    navigationView.setCheckedItem(R.id.drawer_bmi);
+//                }
+                else if (itemid == R.id.navigation_caculator) {
                     selectedFragment = new CaculatorFragment();
                     navigationView.setCheckedItem(R.id.drawer_caculator);
                 }
+
                 getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment, selectedFragment).commit();
                 if (selectedFragment != null) {
                     getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment, selectedFragment).commit();
