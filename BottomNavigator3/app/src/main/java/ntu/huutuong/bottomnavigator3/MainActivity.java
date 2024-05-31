271ed696c9ea8568443dd5148111e331b6622865
Hữu Tường <hunter.19oct@gmail.com>
2024-05-31 22:11:30 +0700

    Update

--- BottomNavigator3/app/src/main/java/ntu/huutuong/bottomnavigator3/MainActivity.java
+++ BottomNavigator3/app/src/main/java/ntu/huutuong/bottomnavigator3/MainActivity.java
@@ -3,12 +3,8 @@ package ntu.huutuong.bottomnavigator3;
 import android.os.Bundle;
 import android.view.MenuItem;
 
-import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -34,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
                     selectedFragment = new HomeFragment();
                 else if (itemid == R.id.nav_search)
                     selectedFragment = new CaculatorFragment();
-                else if (itemid == R.id.nav_notify)
+                else if (itemid == R.id.nav_list)
                     selectedFragment = new RecycerviewFragment();
                 else if (itemid == R.id.nav_profile)
                     selectedFragment = new ProfileFragment();
